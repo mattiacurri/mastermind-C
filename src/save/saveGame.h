c5dfc458bb3ea8f3e7e112a54065e513e8d7de4e
mattiacurri <mattia2002@gmail.com>
2022-05-26 20:01:42 +0200

    UPDATE - Organizzazione dei file - README

--- src/save/saveGame.h
+++ src/save/saveGame.h
@@ -1,7 +1,7 @@
 #ifndef _SAVE_GAME_H
 #define _SAVE_GAME_H
 
-#define PATH "C:\\eclipse\\workspace\\mastermind18-05\\src\\save\\savedMatch\\match.dat"
+#define PATH ""
 
 void saveGame(Match match);
 void loadGame();
